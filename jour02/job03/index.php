9a6b056c656a37f9746b9cdee07b2fea96d06f66
Khiter <yanis.khiter@laplateforme.io>
2021-11-03 14:53:14 +0100

    jour02-job3

---
+++ jour02/job03/index.php
@@ -0,0 +1,26 @@
+<?php
+
+
+        for($x = 0; $x <= 100; $x++){ 
+
+        if ($x == 42){
+            echo 'La Plateforme_ <br/>';}
+       
+
+        else if ($x >0 && $x <= 20){
+            echo '<i>' .$x .'</i><br/>' ;}
+
+
+        else if ($x >24 && $x <= 50){
+            echo '<u>' .$x .'</u><br/>' ;}
+
+        else {
+            echo $x. '<br/>';}
+
+    }
+
+
+
+
+
+
