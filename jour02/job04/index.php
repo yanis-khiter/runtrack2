382d481fc3a28b2b76b1c3757e96e9b8ccbdae5a
Khiter <yanis.khiter@laplateforme.io>
2021-11-03 16:29:54 +0100

    jour02-job04

--- jour02/job04/index.php
+++ jour02/job04/index.php
@@ -3,28 +3,19 @@
 for($x = 0; $x <= 100; $x++){ 
 
     
-    // MODULO 3
-
-    if ($x%3 == 0){
+    
+    if ($x%5 == 0 && $x%3== 0){
+        echo 'FizzBuzz<br/>';
+    }
+    elseif ($x%3 == 0){
         echo 'Fizz<br/>';
     }
-
-    else{
-        echo $x. '<br/>' ;
-    }
-
-    // MODULO 5
-
-    if ($x%5 == 0){
+    elseif ($x%5 == 0){
         echo 'Buzz<br/>';
     }
-
     else{
         echo $x. '<br/>' ;
-    }    
-
-    // MODULO 3 et 5
-
+    }
 
 }
 
