86cf66fded5527f6d2e626b2f1223ca07c3fb8de
Khiter <yanis.khiter@laplateforme.io>
2021-11-06 00:06:25 +0100

    j4-job1

--- jour04/job01/index.php
+++ jour04/job01/index.php
@@ -1,2 +1,21 @@
+<!-- Job 01
+
+Développez un algorithme qui affiche le nombre d’arguments GET.
+Tips : Pour tester, créez un formulaire html de type GET avec différents
+input. -->
+
 <?php
+
+var_dump($_GET);
+
+echo count($_GET);
+
+?>
+
+<form method="get">
+<input type="text" name="user">
+<input type="password" name="pasword">
+<input type="submit" name="submit">
+
+</form>
 
---
+++ jour04/job02/index.php
@@ -0,0 +1,10 @@
+<!-- Job 02
+
+Développer un algorithme qui affiche dans un tableau HTML l’ensemble
+des arguments GET et les valeurs associées. Il doit y avoir deux colonnes :
+argument et valeur.
+Tips : Pour tester, créez un formulaire html de type GET avec différents
+input. -->
+
+<?php
+
