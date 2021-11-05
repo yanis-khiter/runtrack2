94d43c780a7fbc6876ffb1b72ba0c17222ef9fb7
Khiter <yanis.khiter@laplateforme.io>
2021-11-05 14:18:55 +0100

    jour03-job04

--- jour03/job04/index.php
+++ jour03/job04/index.php
@@ -7,3 +7,15 @@ nombre de caractères présents dans cette chaîne. -->
 
 <?php
 
+$str = "Dans l'espace, personne ne vous entend crier." ;
+
+        var_dump ($str);
+
+for ($x = 0;isset ($str[$x]) ;$x++) {
+
+}
+
+     echo $x .'<br/>'
+
+   ?>  
+
