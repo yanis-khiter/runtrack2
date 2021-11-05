e825dd64a3691f947cb9b91d2c0f41f476f4992c
Khiter <yanis.khiter@laplateforme.io>
2021-11-05 11:22:47 +0100

    jour04-job06

---
+++ jour03/job04/index.php
@@ -0,0 +1,9 @@
+<!-- Job 04
+
+Créez une string nommée str et affectez y “Dans l'espace, personne ne
+vous entend crier”. Créez un algorithme qui parcourt, compte et affiche le
+nombre de caractères présents dans cette chaîne. -->
+
+
+<?php
+
---
+++ jour03/job06/index.php
@@ -0,0 +1,20 @@
+<!-- Job 06
+
+Créez une chaîne de caractères str qui contient “Les choses que l'on
+possede finissent par nous posseder.”. Créez un algorithme qui parcourt et
+écrit cette chaine à l’envers. -->
+
+
+<?php
+
+$str = "Les choses que l'on possede finissent par nous posseder.";
+
+for($i = -1 ; isset($str[$i]) == true; $i--){
+
+echo $str[$i];
+
+}
+
+?>
+
+
